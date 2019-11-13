e06addc0f75e7735301a0fabd7bfb072333682ce
Allen Ruan <allenruan50@gmail.com>
2019-11-13 15:40:19 -0500

    Add UnitTest Project

---
+++ MovieShop.UnitTest/MovieServiceTest.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieShop.UnitTest
+{
+    [TestClass]
+    public class MovieServiceTest
+    {
+        [TestMethod]
+        public void TestMoviesFromFakeData1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMoviesFromFakeData2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMoviesFromFakeData3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMoviesFromFakeData4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMoviesFromFakeData5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
